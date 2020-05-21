89f6b409630bc4bcc65298618ef3bbc3ce7e7711
abd-diaa <abdelrahman.diaa.g@gmail.com>
2020-05-22 00:05:45 +0200

    Stack Linked List Implementation

--- Stacks/stack.py
+++ Stacks/stack.py
@@ -9,33 +9,19 @@ class Stack:
         self.size = 0
     
     def push(self, value):
-        if self.isEmpty():
-            self.head = Node(value)
-            self.first_node = self.head
-            self.size += 1
-        else:
-            self.node = Node(value)
-            self.head.next = self.node          
-            self.head = self.node
-            self.size += 1
-            
-        
+        self.new_node = Node(value)
+        self.new_node.next = self.head
+        self.head = self.new_node
+        self.size += 1
+                 
     def pop(self):
         if self.isEmpty():
-            print("Nothing to pop stack is empty")
-        else:    
-            self.iterator = self.first_node
-            while self.iterator.next != None:
-                self.to_pop = self.iterator
-                self.iterator = self.iterator.next
-                
-            if self.iterator == self.first_node:
-                self.head = None
-                self.size -= 1
-            else:
-                self.head = self.to_pop
-                self.head.next = None   
-                self.size -= 1 
+            print("Nothing to pop") 
+            self.size = 0
+        else:
+            print(self.head.value)
+            self.head = self.head.next
+            self.size -= 1
                                
     def isEmpty(self):
         return self.head == None
@@ -44,11 +30,6 @@ class Stack:
         print("Stack Size: " + str(self.size))
     
     def printStack(self):
-        if self.isEmpty():
-            print("Stack is Empty")
-        else:
-            self.iterator = self.first_node
-            while self.iterator != None:
-                print(self.iterator.value)
-                self.iterator = self.iterator.next
-        
+        while self.head != None:
+            print(self.head.value)
+            self.head = self.head.next
